15010ce0cac9e28073e8d9945ef4770f4645a0e8
Jung Sub Park <87363422+crucial-sub@users.noreply.github.com>
2025-07-15 16:41:21 +0900

    [Silver I] Title: 안전 영역, Time: 680 ms, Memory: 34984 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.py
+++ 백준/Silver/2468. 안전 영역/안전 영역.py
@@ -1,43 +1,39 @@
 import sys
-sys.setrecursionlimit(10000)
+from collections import deque
 
-N = int(sys.stdin.readline())
-data = []
-for _ in range(N):
-    row = list(map(int, sys.stdin.readline().split()))
-    data.append(row)
+input = sys.stdin.readline
 
-def func(h):
-    is_visited = [[False] * N for _ in range(N)]
+N = int(input())
+data = [list(map(int, input().split())) for _ in range(N)]
+max_height = max(map(max, data))
 
-    for i in range(N):
-        for j in range(N):
-            if data[i][j] <= h:
-                is_visited[i][j] = True
+# 이동 방향: 상하좌우
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 
-    def dfs(r, c):
-        if r < 0 or r >= N or c < 0 or c >= N:
-            return
-        if is_visited[r][c]:
-            return
+def bfs(x, y, h, visited):
+    q = deque()
+    q.append((x, y))
+    visited[x][y] = True
 
-        is_visited[r][c] = True
-        dfs(r-1, c)
-        dfs(r+1, c)
-        dfs(r, c-1)
-        dfs(r, c+1)
+    while q:
+        cx, cy = q.popleft()
+        for d in range(4):
+            nx, ny = cx + dx[d], cy + dy[d]
+            if 0 <= nx < N and 0 <= ny < N:
+                if not visited[nx][ny] and data[nx][ny] > h:
+                    visited[nx][ny] = True
+                    q.append((nx, ny))
 
+result = 0
+for h in range(0, max_height + 1):
+    visited = [[False] * N for _ in range(N)]
     count = 0
     for i in range(N):
         for j in range(N):
-            if not is_visited[i][j]:
-                dfs(i, j)
+            if not visited[i][j] and data[i][j] > h:
+                bfs(i, j, h, visited)
                 count += 1
-    return count
+    result = max(result, count)
 
-max_height = max(map(max, data))
-max_area = 0
-for h in range(0, max_height + 1):
-    max_area = max(func(h), max_area)
-
-print(max_area)
+print(result)
