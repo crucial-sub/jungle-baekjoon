9b732593ba3c251f7ef21dcc9960f53b2709110b
Jung Sub Park <87363422+crucial-sub@users.noreply.github.com>
2025-07-17 14:49:21 +0900

    [Bronze I] Title: 더하기 사이클, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/1110. 더하기 사이클/더하기 사이클.py
+++ 백준/Bronze/1110. 더하기 사이클/더하기 사이클.py
@@ -1,12 +1,14 @@
-N = int(input())
+import sys
+N = int(sys.stdin.readline())
 
 count = 0;
 str_n = str(N) if N >= 10 else f'0{N}'
-new_num = str_n
+temp_n = str_n
 while True:
-    sum_n = str(sum(int(digit) for digit in new_num))[-1]
-    right_n = new_num[-1]
-    new_num = f'{right_n}{sum_n}'
+    left_n = str(sum(int(digit) for digit in temp_n))[-1]
+    right_n = temp_n[-1]
+    temp_n = f'{right_n}{left_n}'
     count += 1
-    if new_num == str_n: break
+    if temp_n == str_n: break
+
 print(count)
