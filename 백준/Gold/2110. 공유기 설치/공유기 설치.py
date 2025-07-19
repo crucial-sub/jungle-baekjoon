7aca7a39bc5c20f53347a24875c822c8cb6d3a32
Jung Sub Park <87363422+crucial-sub@users.noreply.github.com>
2025-07-19 12:00:14 +0900

    [Gold IV] Title: 공유기 설치, Time: 216 ms, Memory: 41704 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.py
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.py
@@ -1,6 +1,7 @@
 import sys
 input = sys.stdin.readline
 
+
 # 집 간의 거리를 최소 dist값 만큼 띄우고 공유기를 설치할 때 몇 대까지 설치할 수 있는지 카운팅하는 함수
 # 여기서 dist는 최적화됐다고 가정한 거리
 def count_C(houses, dist):
@@ -27,12 +28,17 @@ def main():
 
     while left <= right: # left가 right보다 커지는 순간이 종료 시점
         mid = (left+right)//2 # 중간값 설정 => while 문이 도는 동안 이 mid 값을 최적화된 x좌표간 거리라 가정할 예정
+                                # 즉, x좌표끼리 mid값 만큼 띄워서 공유기를 설치하면 문제 상황을 해결해줄거라고 가정
+                                # 여기서 문제 상황은 공유기를 적절한 거리만큼 띄워서 원하는 개수만큼 설치하는 것
 
         if count_C(arr_x, mid) >= C: # 최소 mid값만큼 거리를 띄우고 공유기를 설치해봤을 때 C개 만큼 설치가 가능한지 체크 
             ans = mid # 체크를 통과한 mid값은 어느정도 최적화가 된것이 확인됐으니 ans에 할당
-            left = mid + 1 # 더 최적화된 값이 있을 수 있으니 left(시작점)을 증가시켜서 다시 돌려봄
+            left = mid + 1 # 더 최적화된 값(여기선 더 큰 mid값)이 있을 수 있으니
+                           # 다음 루프의 mid값을 늘리기 위해 left(시작점)를 mid+1만큼 증가시키고 다시 돌려봄
         else: 
-            right = mid - 1 # 체크를 못통과했으면 right 값을 줄이고 다시 돌림
+            right = mid - 1 # 체크를 못통과했다는 것은 설치한 공유기 수가 C보다 적다는 뜻이고,
+                            # 설치 가능한 공유기 수를 늘리기 위해서는 mid값을 줄일 필요가 있다.
+                            # right mid-1만큼 줄이면 자연히 다음 루프의 mid값도 줄어듬 
 
     print(ans)
 
