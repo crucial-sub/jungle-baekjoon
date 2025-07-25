8156dc117c198bd7b61a1d7aa31d814ce8502435
Jung Sub Park <87363422+crucial-sub@users.noreply.github.com>
2025-07-25 15:28:11 +0900

    [Gold IV] Title: 이진 검색 트리, Time: 2740 ms, Memory: 35488 KB -BaekjoonHub

--- 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.py
+++ 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.py
@@ -1,7 +1,6 @@
 from collections import defaultdict
 import sys
 input = sys.stdin.readline
-sys.setrecursionlimit(10**6)
 
 class BinaryNode:
     def __init__(self, data=None):
@@ -13,6 +12,7 @@ class BinarySearchTree:
     def __init__(self):
         self.root = None
 
+    # 삽입 메서드
     def inser_iter(self, node_val):
         node = BinaryNode(node_val)
         if not self.root:
@@ -34,18 +34,34 @@ class BinarySearchTree:
                         break
 
     # 후위 순회 (왼쪽 -> 오른쪽 -> 루트)
-    def postorder(self):
-        self._postorder_recursive(self.root)
+    def postorder_iter(self):
+        if not self.root:
+            return
+        
+        stack1 = [self.root]
+        stack2 = []
 
-    def _postorder_recursive(self, node):
-        if node:
-            self._postorder_recursive(node.left)
-            self._postorder_recursive(node.right)
+        while stack1:
+            # stack1에서 노드를 꺼내 stack2에 넣는다
+            node = stack1.pop()
+            stack2.append(node)
+
+            # 꺼낸 노드의 자식들을 (존재한다면) stack1에 넣는다
+            # ★★★ 왼쪽 먼저, 그 다음 오른쪽을 넣는 것이 핵심! ★★★
+            if node.left:
+                stack1.append(node.left)
+            if node.right:
+                stack1.append(node.right)
+
+        # stack2에 쌓인 노드들을 역순으로 출력하면 후위 순회 완성!
+        while stack2:
+            node = stack2.pop()
             print(node.data)
 
 def main():
     # 처음에 트리 생성
     bst = BinarySearchTree()
+
 
     while True:
         try:
@@ -54,7 +70,7 @@ def main():
         except (ValueError, IndexError):
             break
 
-    bst.postorder()
+    bst.postorder_iter()
         
 
 
