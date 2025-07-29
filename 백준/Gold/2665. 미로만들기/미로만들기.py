f5830016a5395b535a1b0c7d9d6c16921de3a265
Jung Sub Park <87363422+crucial-sub@users.noreply.github.com>
2025-07-29 19:38:50 +0900

    [Gold IV] Title: 미로만들기, Time: 56 ms, Memory: 34968 KB -BaekjoonHub

--- 백준/Gold/2665. 미로만들기/미로만들기.py
+++ 백준/Gold/2665. 미로만들기/미로만들기.py
@@ -4,40 +4,39 @@ input = sys.stdin.readline
 
 def main():
     n = int(input())
-    rooms = [list(str(input().rstrip())) for _ in range(n)]
+    rooms = [list(input().rstrip()) for _ in range(n)]
     dx = [1, 0, -1, 0]
     dy = [0, 1, 0, -1]
-    cost = [[float('inf')]*(n) for _ in range(n)]
+    
+    cost = [[-1] * n for _ in range(n)]
 
-    visited = set()
     q = deque()
 
-    visited.add((0,0,0))
-    q.append((0,0,0))
+    # 시작점 설정
     cost[0][0] = 0
+    q.append((0, 0))
 
     while q:
-        cur_cost ,cur_x, cur_y = q.popleft()
-        if cur_cost > cost[cur_x][cur_y]:
-            continue
-        for dir in range(4):
-            nx = cur_x + dx[dir]
-            ny = cur_y + dy[dir]
+        cur_x, cur_y = q.popleft()
 
-            if nx < 0 or nx >= n or ny < 0 or ny >= n:
-                continue
-            if (nx, ny) in visited:
-                continue
+        for i in range(4):
+            nx = cur_x + dx[i]
+            ny = cur_y + dy[i]
 
-            visited.add((nx, ny))
-            if rooms[nx][ny] == '0':
-                if cur_cost + 1 < cost[nx][ny]:
-                    cost[nx][ny] = cur_cost + 1
-                    q.append((cur_cost+1, nx, ny))
-            elif rooms[nx][ny] == '1':
-                if cur_cost < cost[nx][ny]:
-                    cost[nx][ny] = cur_cost
-                    q.appendleft((cur_cost, nx, ny))
+            # 맵 경계 체크
+            if not (0 <= nx < n and 0 <= ny < n):
+                continue
+            
+            # 아직 방문하지 않은 곳이라면 (cost가 -1이라면)
+            if cost[nx][ny] == -1:
+                # 하얀 방일 경우
+                if rooms[nx][ny] == '1':
+                    cost[nx][ny] = cost[cur_x][cur_y]
+                    q.appendleft((nx, ny)) # 비용이 0인 경로는 우선적으로 탐색 (앞에 추가)
+                # 검은 방일 경우
+                else:
+                    cost[nx][ny] = cost[cur_x][cur_y] + 1
+                    q.append((nx, ny)) # 비용이 1인 경로는 나중에 탐색 (뒤에 추가)
 
     print(cost[n-1][n-1])
 
