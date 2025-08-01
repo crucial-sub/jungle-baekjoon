f471c64e9057ac7b6d7402828d1c9ecf05e46e35
Jung Sub Park <87363422+crucial-sub@users.noreply.github.com>
2025-08-01 17:21:45 +0900

    [Gold V] Title: 평범한 배낭, Time: 1132 ms, Memory: 36264 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -1,29 +1,19 @@
 import sys
 input = sys.stdin.readline
 
-def main():
+def main(): 
     N, K = map(int, input().split())
-    W = []
-    V = []
+    
+    items = []
     for _ in range(N):
-        w, v = map(int, input().split())
-        W.append(w)
-        V.append(v)
+        items.append(list(map(int, input().split())))
     
-    dp = [[0]*(K+1) for _ in range(N+1)]
+    dp = [0] * (K + 1)
 
-    for i in range(1,N+1):
-        cur_w = W[i-1]
-        cur_v = V[i-1]
-        for w in range(1,K+1):
-            if cur_w > w:
-                dp[i][w] = dp[i-1][w]
-            else:
-                dont_pack = dp[i-1][w]
-                pack = cur_v + dp[i-1][w-cur_w]
-                dp[i][w] = max(dont_pack, pack)
-    
-    print(dp[N][K])
+    for cur_w, cur_v in items:
+        for w in range(K, cur_w - 1, -1):
+            dp[w] = max(dp[w], cur_v + dp[w - cur_w])
+    print(dp[K])
 
 if __name__ == "__main__":
     main()
