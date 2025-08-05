070ea58d5bc81af8a12a049fd2663d8240b8043c
Jung Sub Park <87363422+crucial-sub@users.noreply.github.com>
2025-08-05 12:46:47 +0900

    [Silver IV] Title: 스택, Time: 44 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.py
+++ 백준/Silver/10828. 스택/스택.py
@@ -8,37 +8,39 @@ class Stack:
     def push(self, x):
         """스택 맨 위에 x 추가"""
         self._data.append(x)
-
+    
     def pop(self):
-        """스택 맨 위 요소 제거 후 반환"""
+        """스택 맨 위 요소 제거 후 해당 요소 출력, 제거할 요소가 없을 시 -1 출력"""
         if self.empty():
             return -1
         return self._data.pop()
 
+    def size(self):
+        """스택 내 요소 갯수 출력"""
+        return len(self._data)
+
+    def empty(self):
+        """스택이 비어있을 시 1, 아니면 0을 출력"""
+        return 0 if self._data else 1
+
     def top(self):
-        """스택 맨 위 요소 확인"""
+        """스택 맨 위 요소 출력, 비어있을 시 -1 출력"""
         if self.empty():
             return -1
         return self._data[-1]
 
-    def empty(self):
-        """스택이 비어 있는지 검사"""
-        return 1 if not self._data else 0
-
-    def size(self):
-        """스택 크기 확인"""
-        return len(self._data)
-
 def main():
     N = int(input())
     stack = Stack()
+
     for _ in range(N):
-        oper = input().rstrip()
-        if 'push' in oper:
-            b = oper[5:]
-            stack.push(b)
+        instruction = input().split()
+        if len(instruction) == 2:
+            stack.push(instruction[1])
         else:
-            print(getattr(stack, oper)())
+            if hasattr(stack, instruction[0]):
+                method = getattr(stack, instruction[0])
+                print(method())
 
 if __name__ == "__main__":
     main()
