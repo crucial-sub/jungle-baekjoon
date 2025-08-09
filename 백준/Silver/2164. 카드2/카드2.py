be841711d68240e9da7f514401901aa71a2cb9e7
Jung Sub Park <87363422+crucial-sub@users.noreply.github.com>
2025-08-09 21:20:21 +0900

    [Silver IV] Title: 카드2, Time: 136 ms, Memory: 51856 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.py
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -3,15 +3,20 @@ import sys
 input = sys.stdin.readline
 
 def main():
-    N = int(input())
-    q = deque(range(1,N+1))
-    while len(q)>1:
-        q.popleft()     # 맨위 버리기
-        if len(q) == 1:
-            break
-        a = q.popleft() # 두번째 카드를 빼서,
-        q.append(a)     # 맨 아래로 옮기기
-    print(q[0])
+    N = int(input());
+    if(N == 1):
+        print(1);
+        return;
 
+    q = deque(range(1,N+1));
+
+    while q:
+        q.popleft();
+        if len(q) <= 1:
+            break;
+        a = q.popleft();
+        q.append(a);
+    print(q[0]);
+    
 if __name__ == "__main__":
     main()
