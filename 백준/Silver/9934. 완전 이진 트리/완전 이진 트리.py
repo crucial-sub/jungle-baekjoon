4f69dc9ddbf5f96275537b23b047c07a22fe043e
Jung Sub Park <87363422+crucial-sub@users.noreply.github.com>
2025-08-25 03:27:46 +0900

    [Silver I] Title: 완전 이진 트리, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/9934. 완전 이진 트리/완전 이진 트리.py
+++ 백준/Silver/9934. 완전 이진 트리/완전 이진 트리.py
@@ -3,23 +3,16 @@ input = sys.stdin.readline
 
 def main():
     K = int(input());
-    path = list(map(int, input().split()));
     tree = [[] for _ in range(K)];
+    path = list(map(int, input().split()));
 
-    def find_nodes(nodes, level):
-        if not nodes:
-            return;
+    for i in range(K):
+        order = 2 ** (K - i -1);
+        inc = 2 ** (K - i);
+        while order < (2 ** K):
+            tree[i].append(path[order-1]);
+            order += inc;
 
-        mid_idx = len(nodes) // 2
-        root = nodes[mid_idx];
-        tree[level].append(root);
-
-        if level < (K - 1):
-            find_nodes(nodes[:mid_idx], level + 1)
-            find_nodes(nodes[mid_idx+1:], level + 1)
-
-    find_nodes(path, 0);
-    
     for depth in tree:
         print(*depth);
 
