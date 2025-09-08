7fd06f73196bf6b2e6825267afc488369a61b729
Jung Sub Park <87363422+crucial-sub@users.noreply.github.com>
2025-09-09 08:17:59 +0900

    [Silver V] Title: 거스름돈, Time: 40 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Silver/14916. 거스름돈/거스름돈.py
+++ 백준/Silver/14916. 거스름돈/거스름돈.py
@@ -1,25 +1,20 @@
 import sys
 input = sys.stdin.readline
 
+
 def main():
     n = int(input())
+
     if n == 1 or n == 3:
         print(-1)
         return
 
-    ans = 0
+    five = (n // 5) - 1 if (n % 5) & 1 else (n // 5)
+    five_remain = n - 5 * five
+    two = five_remain // 2
 
-    if n%5 == 0:
-        ans += n//5
-    else:
-        if (n%5) % 2 == 0:
-            ans += n//5
-            ans += (n%5)//2
-        else:
-            ans += (n//5)-1
-            ans += ((n%5)+5)//2
-    
-    print(ans)
+    print(five + two)
+
 
 if __name__ == "__main__":
     main()
