7a202f88a41883dc35dbdca3b4bfc3487e880dcd
Jung Sub Park <87363422+crucial-sub@users.noreply.github.com>
2025-10-20 23:40:12 +0900

    [level 2] Title: 할인 행사, Time: 72.36 ms, Memory: 16.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.py
@@ -0,0 +1,27 @@
+from collections import defaultdict
+
+def solution(want, number, discount):
+    
+    answer = 0
+    len_d = len(discount)
+    
+    for i in range(len_d-9):
+        discounts = discount[i:i+10]
+        if set(want) != set(discounts):
+            continue
+        
+        cnt_discount = defaultdict(int)
+        for d in discounts:
+            cnt_discount[d] += 1
+        
+        passed = True
+        for j,w in enumerate(want):
+            if cnt_discount[w] != number[j]:
+                passed = False
+                break
+                
+        if passed:
+            answer += 1
+        
+        
+    return answer
